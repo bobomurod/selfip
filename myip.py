eee2d971979050d209e195d58b864f76e5bb6c36
bobomurod <bobomurod.boboev@yandex.ru>
2018-05-24 20:03:30 +0300

    oneline version

--- myip.py
+++ myip.py
@@ -4,3 +4,7 @@ import json
 response = requests.get('https://ipinfo.io')
 data = json.loads(response.text)
 print (data['ip'])
+
+# oneline version
+
+print(json.loads(requests.get('https://ipinfo.io').text)['ip'])
